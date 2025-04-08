5b6a8bddd984aab0424326ef6f8900bcb4903c46
Sasha <kkkkkkp_16@mail.ru>
2025-04-08 18:15:30 +0300

    Update submodule

--- TestNelderMead/Test1.cs
+++ TestNelderMead/Test1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
 
 
 namespace TestNelderMead
@@ -8,14 +10,44 @@ namespace TestNelderMead
     [TestClass]
     public class NelderMeadTests
     {
-        [DllImport("C:\\Users\\Admin\\source\\repos\\TestNelderMead\\TestNelderMead\\NelderMead.dll", CallingConvention = CallingConvention.StdCall)]
+        private static string GetDllPath(string dllName)
+        {
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string relativePath = Path.Combine("..", "..", "..", "NelderMead_dll", "build_x64", "Release", "NelderMead.dll");
+            string fullPath = Path.GetFullPath(Path.Combine(baseDirectory, relativePath));
+
+            // Вывод пути для отладки
+            Console.WriteLine($"DLL Path: {fullPath}");
+
+            return fullPath;
+        }
+
+        [DllImport("NelderMead.dll", CallingConvention = CallingConvention.StdCall)]
         static extern double NM_addition(double number_one, double number_two);
-        [DllImport("C:\\Users\\Admin\\source\\repos\\TestNelderMead\\TestNelderMead\\NelderMead.dll", CallingConvention = CallingConvention.StdCall)]
+
+        [DllImport("NelderMead.dll", CallingConvention = CallingConvention.StdCall)]
         static extern double NM_subtraction(double number_one, double number_two);
-        [DllImport("C:\\Users\\Admin\\source\\repos\\TestNelderMead\\TestNelderMead\\NelderMead.dll", CallingConvention = CallingConvention.StdCall)]
+
+        [DllImport("NelderMead.dll", CallingConvention = CallingConvention.StdCall)]
         static extern double NM_multiplication(double number_one, double number_two);
-        [DllImport("C:\\Users\\Admin\\source\\repos\\TestNelderMead\\TestNelderMead\\NelderMead.dll", CallingConvention = CallingConvention.StdCall)]
+
+        [DllImport("NelderMead.dll", CallingConvention = CallingConvention.Cdecl)]
         static extern double NM_division(double number_one, double number_two);
+
+        static NelderMeadTests()
+        {
+            string dllPath = GetDllPath("NelderMead.dll");
+            string dllDirectory = Path.GetDirectoryName(dllPath);
+
+            // Устанавливаем каталог для поиска DLL
+            if (!SetDllDirectory(dllDirectory))
+            {
+                throw new Exception($"Failed to set DLL directory: {dllDirectory}");
+            }
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool SetDllDirectory(string lpPathName);
 
         [TestMethod]
         public void Test_NM_ReturnsSum()
@@ -52,7 +84,7 @@ namespace TestNelderMead
         {
             double divisible = 10.0;
             double divisor = 2.0;
-
+           
             double result = NM_division(divisible, divisor);
 
             Assert.AreEqual(5.0, result);
