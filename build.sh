a0759d95d88d28562b7ff87fe491ee30798767c1
Sergho <sergche04@gmail.com>
2025-05-01 14:40:44 +0300

    refactor: rename `wrapper_build` to just `wrapper`

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-mkdir -p ./TestForParser/wrapper_build
-swig -csharp -c++ -outdir ./TestForParser/wrapper_build -o ./NelderMead_dll/dll/wrapper.cxx ./Wrapper/expression_tree.i
+mkdir -p ./TestForParser/wrapper
+swig -csharp -c++ -outdir ./TestForParser/wrapper -o ./NelderMead_dll/dll/wrapper.cxx ./Wrapper/expression_tree.i
 
 mkdir -p ./build
 cmake -B ./build -S ./NelderMead_dll/dll
