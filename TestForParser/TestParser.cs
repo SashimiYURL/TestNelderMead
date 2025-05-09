a6d29f419c0fe27332668cd9a6a5ca2cd8177497
Sasha <kkkkkkp_16@mail.ru>
2025-05-09 16:29:09 +0300

    add critical tests

--- TestForParser/TestParser.cs
+++ TestForParser/TestParser.cs
@@ -29,7 +29,6 @@ namespace TestForParser
         [InlineData("x1+3", new double[] { 1 }, 4.0)]
         [InlineData("10 + x1", new double[] { 12 }, 22.0)]
         [InlineData("3*x1 + 13*x2 - 10", new double[] { 5.0, 2.0 }, 31.0)]
-        [InlineData("-5 + x1", new double[] { 2.0 }, -3.0)]
         [InlineData("2 + 3*5", new double[] { }, 17.0)]
         public void ExpressionWithConstant(string expression, double[] variables, double result)
             => CheckExpression(expression, variables, result);
@@ -78,7 +77,7 @@ namespace TestForParser
         [InlineData("sin(x1)", new double[] { 0 }, 0.0)]
         [InlineData("cos(x1)", new double[] { 0 }, 1.0)]
         [InlineData("abs(x1)", new double[] { -4 }, 4.0)]
-        [InlineData("sqrt(x1)", new double[] { 9.0 }, 3.0)]
+        [InlineData("sqrt(x1)+1", new double[] { 9.0 }, 4.0)]
         //[InlineData("exp(x1)", new double[] {0}, 1.0)]
         public void MathFunctions(string expression, double[] variables, double result)
             => CheckExpression(expression, variables, result);
@@ -99,6 +98,80 @@ namespace TestForParser
             double actual = tree.evaluate([.. variables]);
             Assert.Equal(result, actual);
         }
+    }
+
+    public class TestParserExceptions
+    {
+        private void ExceptionEvaluteCatchingCheker(string expression, double[] variables, string errorMessage)
+        {
+            var tree = ExpressionTree.create_tree(expression);
+
+            var exception = Assert.Throws<ApplicationException>(() => tree.evaluate([.. variables]));
+            Assert.Equal(errorMessage, exception.Message);
+        }
+
+        private void ExceptionCreateCatchingCheker(string expression, double[] variables, string errorMessage)
+        {
+
+            var exception = Assert.Throws<ApplicationException>(() => ExpressionTree.create_tree(expression));
+            Assert.Equal(errorMessage, exception.Message);
+        }
+
+        [Theory]
+        [InlineData("x1/x2", new double[] { 10.0, 0.0 }, "division by zero.")]
+        [InlineData("x1/(x2-x3)", new double[] { 10.0, 5.0, 5.0 }, "division by zero.")]
+        [InlineData("1/0", new double[] {}, "division by zero.")]
+        public void DivideByZeroException(string expression, double[] variables, string errorMessage)
+            => ExceptionEvaluteCatchingCheker(expression, variables, errorMessage);
+
+        [Theory]
+        [InlineData("x1+6", new double[] { 50.0, 2.0 }, "The number of variables is incorrect")]
+        [InlineData("x1+x2", new double[] { 50.0, 2.0, 3.5 }, "The number of variables is incorrect")]
+        [InlineData("x1-x2", new double[] { 50.0}, "The number of variables is incorrect")]
+        [InlineData("x1+x2-x3", new double[] {}, "The number of variables is incorrect")]
+        public void IncorrectVariablesNumbers(string expression, double[] variables, string errorMessage)
+            => ExceptionEvaluteCatchingCheker(expression, variables, errorMessage);
+
+        [Theory]
+        [InlineData("x y", null, "Invalid expression string")]
+        [InlineData("2 3 +", null, "Invalid expression string")]
+        [InlineData("x + * y", null, "Invalid expression string")]
+        [InlineData("a / / b", null, "Invalid expression string")]
+        [InlineData("(x + y", null, "Invalid expression string")]
+        [InlineData("a * (b + c))", null, "Invalid expression string")]
+        [InlineData("()", null, "Invalid expression string")]
+        [InlineData("x + ()", null, "Invalid expression string")]
+        public void SyntaxException(string expression, double[] variables, string errorMessage)
+            => ExceptionCreateCatchingCheker(expression, variables, errorMessage);
+
+        [Theory]
+        [InlineData(".", null, "Invalid expression string")]
+        [InlineData("123.", null, "Invalid expression string")]
+        [InlineData("1.2.3", null, "Invalid expression string")]
+        [InlineData("1#2", null, "Invalid expression string")]
+        [InlineData("a$ + b@", null, "Invalid expression string")]
+        [InlineData("", null, "Invalid expression string")] 
+        public void IncorrectNumbers(string expression, double[] variables, string errorMessage)
+            => ExceptionCreateCatchingCheker(expression, variables, errorMessage);
+
+        [Theory]
+        [InlineData("foo(x)", null, "Invalid expression string")] 
+        [InlineData("bar(1, 2)", null, "Invalid expression string")]
+        [InlineData("sine(x)", null, "Invalid expression string")] 
+        [InlineData("sqrtt(y)", null, "Invalid expression string")]
+        public void IncorrectNamesAndFunction(string expression, double[] variables, string errorMessage)
+            => ExceptionEvaluteCatchingCheker(expression, variables, errorMessage);
+
+        [Theory]
+        [InlineData("x1+x2", null, "Invalid expression string")]
+        public void ArgumentNullException(string expression, double[] variables, string errorMessage)
+        {
+            var tree = ExpressionTree.create_tree(expression);
+
+            var exception = Assert.Throws<NullReferenceException>(() => tree.evaluate([.. variables]));
+            Assert.Equal(errorMessage, exception.Message);
+        }
+
     }
 
 }
